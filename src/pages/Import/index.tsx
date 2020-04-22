028373a5e0609893fda33bdd88bc5a3b3c7ab83f
Schlanger <leoschlanger@gmail.com>
2020-04-22 10:03:48 -0300

    challenge done

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,29 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    await uploadedFiles.map(async uploadedFile => {
+      data.append('file', uploadedFile.file, uploadedFile.name);
 
-    try {
-      // await api.post('/transactions/import', data);
-    } catch (err) {
-      // console.log(err.response.error);
-    }
+      try {
+        await api.post('transactions/import', data);
+      } catch (err) {
+        console.log(err.response.error);
+      }
+    });
+
+    history.push('/');
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const uploadedFilesFormatted = files.map(file => ({
+      file,
+      name: file.name,
+      readableSize: filesize(file.size),
+    }));
+
+    setUploadedFiles(uploadedFilesFormatted);
   }
 
   return (
